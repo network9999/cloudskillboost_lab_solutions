357b8e90994abeed236e42268e08765db0042b49
network9999 <150242717+network9999@users.noreply.github.com>
2024-06-21 13:04:57 +0530

    Update gsp073.sh

--- JuneArcade_week1/gsp073.sh
+++ JuneArcade_week1/gsp073.sh
@@ -2,7 +2,7 @@ export PROJECT_ID=$(gcloud config get-value project)
 
 gsutil mb -l $REGION -c Standard gs://$PROJECT_ID
 
-curl -O https://https://github.com/network9999/cloudskillboost_lab_solutions/blob/main/JuneArcade_week1/kitten.png
+curl -O https://github.com/network9999/cloudskillboost_lab_solutions/blob/main/JuneArcade_week1/kitten.png
 
 gsutil cp kitten.png gs://$PROJECT_ID/kitten.png
 
