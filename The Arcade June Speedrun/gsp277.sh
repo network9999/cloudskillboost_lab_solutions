003137ea4f805ee56c8ab751775fc04b080e2aae
network9999 <150242717+network9999@users.noreply.github.com>
2024-06-26 10:40:22 +0530

    Create gsp277.sh

---
+++ The Arcade June Speedrun/gsp277.sh
@@ -0,0 +1,9 @@
+export BUCKET="$(gcloud config get-value project)"         
+
+gsutil mb -p $BUCKET gs://$BUCKET-bucket
+
+curl -LO https://github.com/network9999/cloudskillboost_lab_solutions/blob/main/The%20Arcade%20June%20Speedrun/demo-image.jpg
+
+gsutil cp demo-image.jpg gs://$BUCKET-bucket/demo-image.jpg
+
+gsutil acl ch -u allUsers:R gs://$BUCKET-bucket/demo-image.jpg
